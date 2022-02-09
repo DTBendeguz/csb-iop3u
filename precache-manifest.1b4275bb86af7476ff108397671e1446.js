06e3794d7b888ed4cacc57dbebec47af15c2cc8f
Code Sandbox <hello@codesandbox.io>
2022-02-09 20:04:23 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1b4275bb86af7476ff108397671e1446.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde1715ae0933cb4a8a7e36ca12259b6",
+    "url": "https://dtbendeguz.github.io/csb-iop3u/index.html"
+  },
+  {
+    "revision": "5c4bcea3094dfb8dc0e8",
+    "url": "https://dtbendeguz.github.io/csb-iop3u/static/js/2.cad228ae.chunk.js"
+  },
+  {
+    "revision": "c62dab05f4144f071a98",
+    "url": "https://dtbendeguz.github.io/csb-iop3u/static/js/main.01ce44f2.chunk.js"
+  },
+  {
+    "revision": "7d6e4c1500409992ffae",
+    "url": "https://dtbendeguz.github.io/csb-iop3u/static/js/runtime-main.c55e257f.js"
+  }
+]);
